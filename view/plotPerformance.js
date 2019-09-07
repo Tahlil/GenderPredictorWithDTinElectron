a3eb4e77cfcd036a96e2f435309ff2b3541b9b7d
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-09-07 17:40:11 +0600

    Show result in table

--- view/plotPerformance.js
+++ view/plotPerformance.js
@@ -1,14 +1,14 @@
-function plotResult(result, currentNumberPlot){
-//   var table = new Tabulator("#example-table", {
-//     height:"311px",
-//     layout:"fitColumns",
-//     reactiveData:true, //turn on data reactivity
-//     data:tabledata, //load data into table
-//     columns:[
-//         {title:"Name", field:"name", sorter:"string", width:200},
-//         {title:"Predicted Gender", field:"predicted", sorter:"string"},
-//         {title:"Actual Gender", field:"actual", sorter:"string"},
-//         {title:"Success", field:"hasPredicted", formatter:"tickCross", align:"center", width:100}
-//     ],
-// });
+function plotResult(tabledata, currentNumberPlot){
+  var table = new Tabulator("#res-table"+ currentNumberPlot, {
+    height:"311px",
+    layout:"fitColumns",
+    //reactiveData:true, //turn on data reactivity
+    data:tabledata, //load data into table
+    columns:[
+        {title:"Name", field:"name", sorter:"string", width:200},
+        {title:"Predicted Gender", field:"predicted", sorter:"string"},
+        {title:"Actual Gender", field:"actual", sorter:"string"},
+        {title:"Success", field:"hasPredicted", formatter:"tickCross", align:"center", width:100}
+    ],
+});
 }
